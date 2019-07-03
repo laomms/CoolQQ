c11774895e8963db85423fc0ba74d7ac90fa9e71
Tony <laomms@gmail.com>
2019-07-03 19:05:08 +0700

    Update sougouOCR.vb

--- sougouOCR.vb
+++ sougouOCR.vb
@@ -55,3 +55,139 @@ Public Sub SougouOCR(ImagePath As String)
         End Try
         Return bytedata
     End Function
+
+ Public Function OCR_sougou_Content_Length(img As Image) As Byte()
+        On Error Resume Next
+        Dim bytes = Encoding.UTF8.GetBytes("------WebKitFormBoundary8orYTmcj8BHvQpVU" & vbNewLine & "Content-Disposition: form-data; name=pic; filename=pic.jpg" & vbNewLine & "Content-Type: image/jpeg" & vbNewLine & vbNewLine)
+        Dim Array = ImgToBytes(img)
+        Dim bytes2 = Encoding.UTF8.GetBytes(vbNewLine & "------WebKitFormBoundary8orYTmcj8BHvQpVU--" & vbNewLine)
+        Dim array2 As Byte()
+        ReDim array2(bytes.Length + Array.Length + bytes2.Length)
+        bytes.CopyTo(array2, 0)
+        Array.CopyTo(array2, bytes.Length)
+        bytes2.CopyTo(array2, bytes.Length + Array.Length)
+        Return array2
+    End Function
+
+Public Function OCR_sougou_Image_Bytes(url As String) As Byte()
+        On Error Resume Next
+        Dim bytes = Encoding.UTF8.GetBytes("------WebKitFormBoundary8orYTmcj8BHvQpVU" & vbNewLine & "Content-Disposition: form-data; name=pic; filename=pic.jpg" & vbNewLine & "Content-Type: image/jpeg" & vbNewLine & vbNewLine)
+        Dim Array = urlimageTobyte(url)
+        Dim bytes2 = Encoding.UTF8.GetBytes(vbNewLine & "------WebKitFormBoundary8orYTmcj8BHvQpVU--" & vbNewLine)
+        Dim array2 As Byte()
+        ReDim array2(bytes.Length + Array.Length + bytes2.Length)
+        bytes.CopyTo(array2, 0)
+        Array.CopyTo(array2, bytes.Length)
+        bytes2.CopyTo(array2, bytes.Length + Array.Length)
+        Return array2
+    End Function
+    Public Function OCR_sougou_SogouPost(url As String, cookie As CookieContainer, content As Byte()) As String
+        Dim result As String = ""
+        Try
+
+
+            Dim httpWebRequest As HttpWebRequest = DirectCast(WebRequest.Create(url), HttpWebRequest)
+            httpWebRequest.Method = "POST"
+            httpWebRequest.CookieContainer = cookie
+            httpWebRequest.Timeout = 10000
+            httpWebRequest.Referer = "http://pic.sogou.com/resource/pic/shitu_intro/index.html"
+            httpWebRequest.ContentType = "multipart/form-data; boundary=----WebKitFormBoundary8orYTmcj8BHvQpVU"
+            httpWebRequest.Accept = "*/*"
+            httpWebRequest.Headers.Add("Origin: http://pic.sogou.com")
+            httpWebRequest.Headers.Add("Accept-Encoding: gzip,deflate")
+            httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+            httpWebRequest.ServicePoint.Expect100Continue = False
+            httpWebRequest.ProtocolVersion = New Version(1, 1)
+            httpWebRequest.ContentLength = content.Length
+            Dim requestStream = httpWebRequest.GetRequestStream()
+            requestStream.Write(content, 0, content.Length)
+            requestStream.Close()
+
+            Try
+                Dim httpWebResponse As HttpWebResponse = httpWebRequest.GetResponse()
+                If httpWebResponse.ContentEncoding.ToLower().Contains("gzip") Then
+                    Using stream As Stream = New System.IO.Compression.GZipStream(httpWebResponse.GetResponseStream, IO.Compression.CompressionMode.Decompress)
+                        Using reader As New StreamReader(stream)
+                            result = reader.ReadToEnd()
+                            Debug.Print(result)
+                            reader.Close()
+                        End Using
+                        stream.Close()
+                    End Using
+                Else
+                    Using stream As Stream = httpWebRequest.GetResponse().GetResponseStream()
+                        Using reader As New StreamReader(stream)
+                            result = reader.ReadToEnd()
+                            Debug.Print(result)
+                            reader.Close()
+                        End Using
+                        stream.Close()
+                    End Using
+                End If
+                httpWebResponse.Close()
+            Catch ex As Exception
+
+            End Try
+        Catch ex As Exception
+
+        End Try
+        Return result
+    End Function
+
+    Public Function OCR_sougou_SogouGet(url As String, cookie As CookieContainer, refer As String) As String
+        Dim result As String = ""
+        Try
+            Dim Text = ""
+            Dim httpWebRequest As HttpWebRequest = DirectCast(WebRequest.Create(url), HttpWebRequest)
+            httpWebRequest.Method = "GET"
+            httpWebRequest.CookieContainer = cookie
+            httpWebRequest.Referer = refer
+            httpWebRequest.Timeout = 10000
+            httpWebRequest.Accept = "application/json"
+            httpWebRequest.Headers.Add("X-Requested-With: XMLHttpRequest")
+            httpWebRequest.Headers.Add("Accept-Encoding: gzip,deflate")
+            httpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8"
+            httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+            httpWebRequest.ServicePoint.Expect100Continue = False
+            httpWebRequest.ProtocolVersion = New Version(1, 1)
+
+
+            Try
+                Dim httpWebResponse As HttpWebResponse = httpWebRequest.GetResponse()
+                If httpWebResponse.ContentEncoding.ToLower().Contains("gzip") Then
+                    Using stream As Stream = New System.IO.Compression.GZipStream(httpWebResponse.GetResponseStream, IO.Compression.CompressionMode.Decompress)
+                        Using reader As New StreamReader(stream)
+                            result = reader.ReadToEnd()
+                            Debug.Print(result)
+                            reader.Close()
+                        End Using
+                        stream.Close()
+                    End Using
+                Else
+                    Using stream As Stream = httpWebRequest.GetResponse().GetResponseStream()
+                        Using reader As New StreamReader(stream)
+                            result = reader.ReadToEnd()
+                            Debug.Print(result)
+                            reader.Close()
+                        End Using
+                        stream.Close()
+                    End Using
+                End If
+                httpWebResponse.Close()
+            Catch ex As Exception
+
+            End Try
+        Catch ex As Exception
+
+        End Try
+        Return result
+    End Function
+
+    Public Function MergeByte(ByVal a As Byte(), ByVal b As Byte(), ByVal c As Byte()) As Byte()
+        On Error Resume Next
+        Dim array = New Byte(a.Length + b.Length + c.Length - 1) {}
+        a.CopyTo(array, 0)
+        b.CopyTo(array, a.Length)
+        c.CopyTo(array, a.Length + b.Length)
+        Return array
+    End Function
