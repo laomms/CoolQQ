782c15c72f2c9b104749ade14706b8ecea5f99ee
Tony <laomms@gmail.com>
2019-10-30 20:21:53 +0700

    Update TecentTranslate.vb

--- TecentTranslate.vb
+++ TecentTranslate.vb
@@ -1,4 +1,4 @@
- Dim TimeStamp As Integer = (DateTime.UtcNow - New DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds
+Dim TimeStamp As Integer = (DateTime.UtcNow - New DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds
             Dim r As System.Random = New Random(System.Environment.TickCount)
             Dim Nonce = r.Next(10000000, 99999999)
             Dim value = HttpUtility.UrlEncode(RichTextBox1.Text, Encoding.UTF8).ToUpper()
@@ -32,3 +32,23 @@
             End Using
             Dim jsons As JObject = JObject.Parse(result)
             Dim szResult As String = jsons.SelectToken("data.target_text").ToString
+
+ Private Function GetSign(ByVal params As SortedDictionary(Of String, String)) As String
+        Dim str = $"{GetUrlValue(params)}&app_key={"lMaNsVDFw6asBJji"}"
+        Using md5csp = New MD5CryptoServiceProvider()
+            Dim temp = Encoding.UTF8.GetBytes(str)
+            temp = md5csp.ComputeHash(temp)
+            Return BitConverter.ToString(temp).Replace("-", "")
+        End Using
+    End Function
+
+    Private Function GetUrlValue(ByVal params As SortedDictionary(Of String, String)) As String
+        Dim sb = New StringBuilder()
+        For Each item In params
+            If sb.Length > 0 Then
+                sb.Append("&"c)
+            End If
+            sb.Append($"{item.Key}={item.Value}")
+        Next item
+        Return sb.ToString()
+    End Function
