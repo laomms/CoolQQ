017f88ae643c90fbfe47ea94481b0c05aa8516f4
Tony <laomms@gmail.com>
2019-10-30 11:55:06 +0700

    Update TecentLanDetect.c

--- TecentLanDetect.c
+++ TecentLanDetect.c
@@ -9,12 +9,12 @@ public string LanguageDetect(string szContet)
 		var Nonce = r.Next(10000000, 99999999);
 		var SecretId = Your SecretId
                 var SecretKey == Your SecretKey
-                var httpRequestMethod == "POST";
+
 		var canonicalUri = "/";
 		var canonicalHeaders = "content-type:application/json" + "\n" + "host:" + "tmt.ap-guangzhou.tencentcloudapi.com" + "\n";
 		var signedHeaders = "content-type;host";
 		var hashedRequestPayload = GenerateSHA256String(jsonText).ToLower();
-		var canonicalRequest = httpRequestMethod + "\n" + canonicalUri + "\n" + "\n" + canonicalHeaders + "\n" + signedHeaders + "\n" + hashedRequestPayload;
+		var canonicalRequest = "POST" + "\n" + canonicalUri + "\n" + "\n" + canonicalHeaders + "\n" + signedHeaders + "\n" + hashedRequestPayload;
 
 		var credentialScope = szDate + "/tmt/tc3_request";
 		var hashedCanonicalRequest = GenerateSHA256String(canonicalRequest).ToLower();
