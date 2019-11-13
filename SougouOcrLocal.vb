8c22baff50269b6af4481b179db8d2afd545279e
Tony <laomms@gmail.com>
2019-11-13 15:03:58 +0700

    Update SougouOcrLocal.vb

--- SougouOcrLocal.vb
+++ SougouOcrLocal.vb
@@ -57,3 +57,50 @@
         End Try
         Return result
     End Function
+
+ Public Function OCR_sougou_SogouPost(url As String, cookie As CookieContainer, content As Byte()) As String
+
+        Dim httpWebRequest As HttpWebRequest = DirectCast(WebRequest.Create(url), HttpWebRequest)
+        httpWebRequest.Method = "POST"
+        httpWebRequest.CookieContainer = cookie
+        httpWebRequest.Timeout = 10000
+        httpWebRequest.Referer = "http://pic.sogou.com/resource/pic/shitu_intro/index.html"
+        httpWebRequest.ContentType = "multipart/form-data; boundary=----WebKitFormBoundary8orYTmcj8BHvQpVU"
+        httpWebRequest.Accept = "*/*"
+        httpWebRequest.Headers.Add("Origin: http://pic.sogou.com")
+        httpWebRequest.Headers.Add("Accept-Encoding: gzip,deflate")
+        httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+        httpWebRequest.ServicePoint.Expect100Continue = False
+        httpWebRequest.ProtocolVersion = New Version(1, 1)
+        httpWebRequest.ContentLength = content.Length
+        Dim requestStream = httpWebRequest.GetRequestStream()
+        requestStream.Write(content, 0, content.Length)
+        requestStream.Close()
+        Dim result As String = ""
+        Try
+            Dim httpWebResponse As HttpWebResponse = httpWebRequest.GetResponse()
+            If httpWebResponse.ContentEncoding.ToLower().Contains("gzip") Then
+                Using stream As Stream = New System.IO.Compression.GZipStream(httpWebResponse.GetResponseStream, IO.Compression.CompressionMode.Decompress)
+                    Using reader As New StreamReader(stream)
+                        result = reader.ReadToEnd()
+                        Debug.Print(result)
+                        reader.Close()
+                    End Using
+                    stream.Close()
+                End Using
+            Else
+                Using stream As Stream = httpWebRequest.GetResponse().GetResponseStream()
+                    Using reader As New StreamReader(stream)
+                        result = reader.ReadToEnd()
+                        Debug.Print(result)
+                        reader.Close()
+                    End Using
+                    stream.Close()
+                End Using
+            End If
+            httpWebResponse.Close()
+        Catch ex As Exception
+
+        End Try
+        Return result
+    End Function
